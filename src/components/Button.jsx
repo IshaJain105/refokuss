759dd44eb0ce9d2ac90994238b78b3c022a6b536
unknown <ishajain105k@gmail.com>
2024-09-20 02:19:57 +0530

    Day 2.2: Work Comp.

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -3,7 +3,7 @@ import { IoIosReturnRight } from "react-icons/io";
 
 const Button = () => {
   return (
-    <div className='w-[10vw] px-4 py-2 bg-white text-zinc-900 rounded-full justify-between flex items-center font-medium text-sm'>
+    <div className='w-[10vw] px-4 py-2 bg-white text-zinc-900 rounded-full justify-around flex items-center font-medium text-sm'>
         <h1>Start a Project</h1>
         <IoIosReturnRight className='w-5 h-5'/>
     </div>
