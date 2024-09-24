47e1bd87784e1cd25b68da93424a0c7c3785534a
unknown <ishajain105k@gmail.com>
2024-09-25 00:01:17 +0530

    Day 6 : ANIMATIONS : Product Component

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 import Button from "./Button";
 
-const Product = ({val}) => {
+const Product = ({val,mover,count}) => {
   return (
-    <div className="w-full py-20 ">
-      <div className="max-w-screen-xl mx-auto flex items-center justify-between">
+    <div className="w-full py-20 h-[23rem] ">
+      <div onMouseEnter={()=>{mover(count)}} className="max-w-screen-xl mx-auto flex items-center justify-between">
         <h1 className="text-6xl capatalize fonr-medium">{val.title}</h1>
         <div className="dets w-1/3">
           <p className="mb-10">
--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,5 +1,6 @@
-import React from "react";
+import React, { useState } from "react";
 import Product from "./Product";
+import { motion } from "framer-motion";
 
 const Products = () => {
   const Products = [
@@ -53,11 +54,65 @@ const Products = () => {
       case: false,
     },
   ];
+
+ 
+
+  const [pos, setPos] = useState(0);
+
+  const mover = (val) => {
+    setPos(val * 23);
+  };
+
   return (
-    <div className="my-20">
+    <div className="my-20 relative ">
       {Products.map((elem, index) => (
-        <Product key={index} val={elem} />
+        <Product key={index} val={elem} mover={mover} count={index} />
       ))}
+      <div className="absolute top-0 w-full h-full pointer-events-none ">
+        <motion.div
+          initial={{ y: pos, x: "-50%" }}
+          animate={{ y: pos + `rem` }}
+          transition={{ ease: [0.76, 0, 0.24, 1], duration: 0.6 }}
+          className="window absolute w-[32rem] h-[23rem] bg-white left-[44%] overflow-hidden"
+        >
+       <motion.div
+            animate={{ y: -pos + `rem` }}
+          transition={{ease: [0.76, 0, 0.24, 1],duration :.5}}
+            className="w-full h-full "
+          > <img src="https://assets-global.website-files.com/6334198f239547d0f9cd84b3/634ef09178195ce0073e38f3_Refokus%20Tools-1.png"></img > </motion.div>
+          <motion.div
+            animate={{ y: -pos + `rem` }}
+          transition={{ease: [0.76, 0, 0.24, 1],duration :.5}}
+            className="w-full h-full "
+          ><img src="https://assets-global.website-files.com/6334198f239547d0f9cd84b3/634ef0accfe1b3e66bc55462_Refokus%20Tools.png"></img ></motion.div>
+          <motion.div
+            animate={{ y: -pos + `rem` }}
+          transition={{ease: [0.76, 0, 0.24, 1],duration :.5}}
+            className="w-full h-full "
+          ><img src="https://assets-global.website-files.com/6334198f239547d0f9cd84b3/634ef0acbc45cb2f4fc5c6b2_Yahoo.png"></img ></motion.div>
+          <motion.div
+            animate={{ y: -pos + `rem` }}
+          transition={{ease: [0.76, 0, 0.24, 1],duration :.5}}
+            className="w-full h-full "
+          ><img src="https://assets-global.website-files.com/6334198f239547d0f9cd84b3/634ef092455ce2cf591e52d1_Rainfall.png"></img ></motion.div>
+          <motion.div
+            animate={{ y: -pos + `rem` }}
+          transition={{ease: [0.76, 0, 0.24, 1],duration :.5}}
+            className="w-full h-full "
+          ><img src="https://assets-global.website-files.com/6334198f239547d0f9cd84b3/634ef0ac7e7179d210dc41f0_Summon.png"></img ></motion.div>
+          <motion.div
+            animate={{ y: -pos + `rem` }}
+          transition={{ease: [0.76, 0, 0.24, 1],duration :.5}}
+            className="w-full h-full "
+          ><img src="https://assets-global.website-files.com/6334198f239547d0f9cd84b3/634ef0af108a465002975acd_Showcase%20Websites%20(1).png"></img ></motion.div>
+          <motion.div
+            animate={{ y: -pos + `rem` }}
+          transition={{ease: [0.76, 0, 0.24, 1],duration :.5}}
+            className="w-full h-full "
+          ><img src="https://assets-global.website-files.com/6334198f239547d0f9cd84b3/634ef0accfe1b3e66bc55462_Refokus%20Tools.png"></img ></motion.div>
+          
+        </motion.div>
+      </div>
     </div>
   );
 };
